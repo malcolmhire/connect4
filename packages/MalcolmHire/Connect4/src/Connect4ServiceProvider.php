66aa2f590039c96e3f2fdd0ba8ebbcaae20c9c8b
Malcolm Hire <malcolmhire07@sky.com>
2016-05-03 21:18:21 +0100

    Added Connect4 Package

---
+++ packages/MalcolmHire/Connect4/src/Connect4ServiceProvider.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace MalcolmHire\Connect4;
+
+use Illuminate\Support\ServiceProvider;
+
+class Connect4ServiceProvider extends ServiceProvider {
+
+    /**
+    * Bootstrap the application services.
+    *
+    * @return void
+    */
+    public function boot()
+    {
+    //
+    }
+
+    /**
+    * Register the application services.
+    *
+    * @return void
+    */
+    public function register()
+    {
+        $this->registerCommands();
+    }
+
+    public function registerCommands()
+    {
+        $this->registerConnect4Command();
+
+        $this->commands(
+            'connect4::commands.play',
+        );
+    }
+
+    public function registerConnect4Command()
+    {
+        $this->app['connect4::commands.play'] = $this->app->share(function($app)
+        {
+            return new Console\Connect4Command;
+        });
+    }
+}
